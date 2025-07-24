613f109f7b1c95fb9fa3a856e2194f11db193767
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-07-24 20:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (9.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        dic = {"(":")","{":"}","[":"]"}
+
+        for i in s:
+            if i in dic.keys():
+                st.append(i)
+            else:
+                if st == []:
+                    return False
+                else:
+                    if dic[st[-1]] == i:
+                        st.pop()
+                    else:
+                        return False
+        if st == []:
+            return True
+        else:
+            return False
