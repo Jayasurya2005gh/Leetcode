4e4fbe671176a7006d55abd1af4032d472e5624d
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-07-27 21:51:57 +0530

    Time: 3 ms (8.06%) | Memory: 18.7 MB (73.63%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,8 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        minise = -1
         for i,n in enumerate(nums):
             if n == target:
                 return i
-        return minise
+        return -1
