aa2a6d6d6db54a73c22d28548f991743b8d476f2
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-07-28 21:58:47 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (25.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,14 +1,9 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        # Take only the first m elements from nums1 (ignore placeholders)
+        
         ans = nums1[:m]
-
-        # Merge with nums2
         res = ans + nums2
+        sort = sorted(res)
 
-        # Sort the merged list
-        res.sort()
-
-        # Update nums1 in-place
         for i in range(len(nums1)):
-            nums1[i] = res[i]
+            nums1[i] = sort[i]
