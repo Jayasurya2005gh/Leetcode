a43cc98d4adde97c6dfa3675f0b8362dc2fb3d40
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-07-28 23:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (43.40%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,9 +1,13 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
         ans = nums1[:m]
         res = ans + nums2
         sort = sorted(res)
 
         for i in range(len(nums1)):
             nums1[i] = sort[i]
+        
