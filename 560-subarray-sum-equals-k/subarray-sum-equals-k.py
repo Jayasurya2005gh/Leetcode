e40a605c3f7295f82ef4e74764970bf1fdd27658
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-07-28 22:52:07 +0530

    Time: 29 ms (70.30%) | Memory: 20.2 MB (98.93%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -4,14 +4,11 @@ class Solution:
         dic = {0:1}
         count = 0
         s = 0
-
         for i in nums:
             s += i
             if s - k in dic:
                 count += dic[s-k]
             dic[s] = dic.get(s,0)+1
         return count
-
-
 
         
