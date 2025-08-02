ac91398e7f0284b970b3f8c72f3d55f4c35b3e78
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-02 09:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.54%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -4,10 +4,10 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        ans = nums1[:m]
-        res = ans + nums2
-        sort = sorted(res)
+        res = nums1[:m]
+        ans = res + nums2
+        sort = sorted(ans)
 
         for i in range(len(nums1)):
             nums1[i] = sort[i]
-        
+        return sort       
