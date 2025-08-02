2e2fc229edd686047976101ba478207c583cb1bb
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-02 09:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (30.38%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-
+        
         res = ""
 
         for i in range(len(strs[0])):
@@ -9,4 +9,3 @@ class Solution:
                     return res
             res += strs[0][i]
         return res
-        
