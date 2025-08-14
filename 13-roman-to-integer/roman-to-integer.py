d67a984be2eaea4827e4e29758a448bc30a81324
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-14 16:36:18 +0530

    Time: 7 ms (39.79%) | Memory: 17.6 MB (90.05%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        dic = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        total = 0
+        prev_val = 0
+
+        for i in reversed(s):
+            curr_val = dic[i]
+            if curr_val < prev_val:
+                total -= curr_val
+            else:
+                total += curr_val
+            prev_val = curr_val
+        return total
