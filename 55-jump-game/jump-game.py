0aafe7352d0316dd3df6b5d33855990393cebe97
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-14 17:16:30 +0530

    Time: 14 ms (79.89%) | Memory: 18.8 MB (25.16%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -3,13 +3,13 @@ class Solution:
 
         last_ind = len(nums)-1
 
+
         for i in range(len(nums)-2,-1,-1):
             if nums[i] + i >= last_ind:
                 last_ind = i
+        
         if last_ind == 0:
             return True
         else:
             return False
-
-
         
