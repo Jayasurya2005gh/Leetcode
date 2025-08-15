478526e2b691b38dcf3e3f78ed66a172f3598896
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-15 11:07:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (67.39%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -6,7 +6,8 @@ class Solution:
         for r in range(len(t)):
             if l < len(s) and s[l] == t[r]:
                 l += 1
-        if l == len(s):
+
+        if len(s) == l:
             return True
         else:
             return False
