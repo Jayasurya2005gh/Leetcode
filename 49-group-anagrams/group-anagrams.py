36e812826719a373423edf3d6d168e15ebc6f669
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-17 18:14:11 +0530

    Time: 10 ms (86.69%) | Memory: 20.6 MB (90.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,9 +4,11 @@ class Solution:
         dic = {}
 
         for i in strs:
-            sort = "".join(sorted(i))
-            if sort not in dic:
-                dic[sort] = [i]
+            sort = sorted(i)
+            sortj = "".join(sort)
+            if sortj not in dic:
+                dic[sortj] = [i]
             else:
-                dic[sort].append(i)
+                dic[sortj].append(i)
         return list(dic.values())
+        
