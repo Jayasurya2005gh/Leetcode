9157a0d1a423d27f105fb30b0eaf662bcbdb967a
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-17 13:19:01 +0530

    Time: 15 ms (89.28%) | Memory: 23.3 MB (44.22%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -2,18 +2,15 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
 
         g = 0
-        t = 0
+        p = 0
+
         if sum(gas) < sum(cost):
             return -1
 
         for i in range(len(gas)):
-            t += gas[i]
-            t -= cost[i]
-
-            if t < 0:
-                t = 0
+            p += gas[i] - cost[i]
+            if p < 0:
+                p = 0
                 g = i+1
         return g
-
-        
         
