68372907b1533a9449875d77e1b32a3fa46a6127
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-19 06:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.42%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,21 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        ans = ""
+        res = []
+        
+
+
+        for i in digits:
+            ans += str(i)
+        ans = str(int(ans) + 1)
+
+        for i in ans:
+            res.append(int(i))
+        
+        return res
+        
+
+        
+
+        
