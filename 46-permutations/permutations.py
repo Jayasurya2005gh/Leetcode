407b5983cfc8a3d3c105d72b182de55a7c71a28a
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-23 21:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (81.87%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -3,6 +3,7 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
         res = []
+
         for i in permutations(nums):
             res.append(list(i))
         return res
