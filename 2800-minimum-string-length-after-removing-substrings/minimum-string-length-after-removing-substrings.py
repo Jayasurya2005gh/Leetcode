8278aff4cccd5ad12c1e3050dd181407ed5a5483
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-08-28 22:19:42 +0530

    Time: 3 ms (86.48%) | Memory: 17.9 MB (33.14%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -3,8 +3,13 @@ class Solution:
 
         res = []
         for i in s:
-            if res != [] and ((res[-1] == 'A' and i == 'B') or (res[-1] == 'C' and i == 'D')):
-                res.pop()
+            if res != []:
+                if res[-1] == 'A' and i == 'B':
+                    res.pop()
+                elif res[-1] == 'C' and i == 'D':
+                    res.pop()
+                else:
+                    res.append(i)
             else:
                 res.append(i)
         return len(res)
