e5e01941dbb3cb027682cce9341290ee7272b719
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-09-12 21:42:06 +0530

    Time: 3 ms (36.30%) | Memory: 17.7 MB (90.99%) - LeetSync

--- 3324-split-the-array/split-the-array.py
+++ 3324-split-the-array/split-the-array.py
@@ -1,12 +1,11 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
 
-        res = []
         set_nums = list(set(nums))
 
         for i in set_nums:
             if nums.count(i) <= 2:
-                res.append(i)
+                continue
             else:
                 return False
         return True
