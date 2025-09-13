76661cc46d8a126023f953393c5e1821c4fc575d
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-09-13 21:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.89%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
+
         res = []
         even = []
         odd = []
 
         for i in set(s):
             res.append(s.count(i))
-
+    
         for i in res:
             if i % 2 == 0:
                 even.append(i)
@@ -17,3 +18,4 @@ class Solution:
             return 0  
 
         return max(odd) - min(even)
+        
