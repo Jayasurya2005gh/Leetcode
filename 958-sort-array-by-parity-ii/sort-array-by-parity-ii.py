fd94f16064466bd1208b0c0bdbb6a698e0c04ba6
Jayasurya2005gh <jayasuryamuruganandham@gmail.com>
2025-10-04 20:13:57 +0530

    Time: 8 ms (46.32%) | Memory: 19.6 MB (17.33%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+
+        odd = []
+        even = []
+        res = []
+
+        for i in nums:
+            if i % 2 == 0:
+                even.append(i)
+            else:
+                odd.append(i)
+        
+        for i in range(len(even)):
+            res.append(even[i])
+            res.append(odd[i])
+    
+        return res
+        
