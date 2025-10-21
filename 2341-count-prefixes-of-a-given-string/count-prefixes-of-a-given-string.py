43629fa328aac1cbfe24f093013c8437eec587d6
Jayasurya KM <jayasuryamuruganandham@gmail.com>
2025-10-21 23:20:58 +0530

    Time: 3 ms (30.74%) | Memory: 18 MB (32.63%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.py
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.py
@@ -1,7 +1,11 @@
 class Solution:
     def countPrefixes(self, words: List[str], s: str) -> int:
+
         count = 0
-        for word in words:
-            if s.startswith(word):
+
+        for i in words:
+            if s.startswith(i):
                 count += 1
+        
         return count
+        
