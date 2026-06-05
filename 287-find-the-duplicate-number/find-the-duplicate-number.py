857e27dcbefa3b7ddfa55e5fb7adb701d03ce460
Jayasurya KM <jayasuryamuruganandham@gmail.com>
2026-06-05 20:43:04 +0530

    Time: 37 ms (23.87%) | Memory: 40.9 MB (6.27%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -2,7 +2,6 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
         dic = {}
-
         for i in nums:
             if i not in dic:
                 dic[i] = 1
@@ -10,8 +9,6 @@ class Solution:
                 dic[i] += 1
 
         for key,values in dic.items():
-            if values >= 2:
+            if values > 1:
                 return key
-                
-
         
