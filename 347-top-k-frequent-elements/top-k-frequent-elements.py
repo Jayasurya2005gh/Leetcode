53c7bd7611b2587169c5e3612e748c7dbdb2d2e8
Jayasurya KM <jayasuryamuruganandham@gmail.com>
2026-06-12 01:34:51 +0530

    Time: 2 ms (91.49%) | Memory: 22.6 MB (99.34%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,18 +2,21 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         dic = {}
-        res = []
 
         for i in nums:
             if i not in dic:
                 dic[i] = 1
             else:
                 dic[i] += 1
-        
-        while len(res) != k:
-            max_key = max(dic, key = dic.get)
-            res.append(max_key)
-            del dic[max_key]
-    
+
+        sorted_dic = dict(sorted(dic.items(), key = lambda x : x[1], reverse = True))
+        res = []
+        ans = 0
+
+        for key in sorted_dic.keys():
+            if ans == k:
+                break
+            else:
+                res.append(key)
+                ans += 1
         return res
-        
