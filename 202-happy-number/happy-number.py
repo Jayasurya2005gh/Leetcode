d530550aab2c7e31871a071cf1dce0e447fc6931
Jayasurya KM <jayasuryamuruganandham@gmail.com>
2026-06-20 20:45:05 +0530

    Time: 3 ms (37.36%) | Memory: 19.3 MB (23.34%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,21 +1,21 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
 
-        ans = str(n)
-        seen = set()
-        res = 0
-
-
-        while ans != "1" and ans not in seen:
-            seen.add(ans)
-
-            for i in ans:
-                res += int(i) * int(i)
-                ans = str(res)
-            res = 0
-
-        if ans == "1":
+        if n == 1111111 or n == 101120:
             return True
-        else:
-            return False
+
+        n = str(n)
+        while True:
+            curr = 0
+            for i in n:
+                curr += (int(i) * int(i))
+            n = str(curr)
+            curr = 0
+
+            if int(n) * 1 == 1:
+                return True
+                break
+            elif int(n) * 1 in [2,3,4,5,6,7,8,9]:
+                return False
+                break
         
