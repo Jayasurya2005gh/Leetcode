6eb17794c7386fbcd13d2e9e278a9da65d5e089d
Jayasurya KM <jayasuryamuruganandham@gmail.com>
2026-07-18 20:05:26 +0530

    Time: 12 ms (25.40%) | Memory: 21.2 MB (49.57%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        nums = sorted(nums)
-        mid_part = len(nums) // 2
-        return nums[mid_part]
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+
+        max_val = max(dic, key = dic.get)
+        return max_val
         
